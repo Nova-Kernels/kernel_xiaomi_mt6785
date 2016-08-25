9524ac7120670abacda9b49e8d935bdad4f9a841
Steve Muckle <smuckle@linaro.org>
2016-08-25 15:59:17 -0700

    ANDROID: sched: cpufreq: use rt_avg as estimate of required RT CPU capacity
    A policy of going to fmax on any RT activity will be detrimental
    for power on many platforms. Often RT accounts for only a small amount
    of CPU activity so sending the CPU frequency to fmax is overkill. Worse
    still, some platforms may not be able to even complete the CPU frequency
    change before the RT activity has already completed.
    
    Cpufreq governors have not treated RT activity this way in the past so
    it is not part of the expected semantics of the RT scheduling class. The
    DL class offers guarantees about task completion and could be used for
    this purpose.
    
    Modify the schedutil algorithm to instead use rt_avg as an estimate of
    RT utilization of the CPU.
    
    Based on previous work by Vincent Guittot <vincent.guittot@linaro.org>.
    
    Change-Id: I1ed605a3e2512a94d34217a8e57c3fd97cca60be
    Signed-off-by: Steve Muckle <smuckle@linaro.org>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    [smuckle@google.com: incorporated the following patch from
     chris.redpath@arm.com:
     ANDROID: cpufreq/schedutil: use boosted_cpu_util for PELT to match WALT]
    Bug: 79886566
    Signed-off-by: Steve Muckle <smuckle@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -251,18 +251,27 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 }
 #endif
 
-static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
+static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu,
+			   u64 time)
 {
-	unsigned long max_cap;
+	struct rq *rq = cpu_rq(cpu);
+	unsigned long max_cap, rt;
+	s64 delta;
 
 	max_cap = arch_scale_cpu_capacity(NULL, cpu);
+	*max = max_cap;
 
 	*util = boosted_cpu_util(cpu);
-	if (idle_cpu(cpu))
-		*util = 0;
 
-	*util = min(*util, max_cap);
-	*max = max_cap;
+	if (likely(use_pelt())) {
+		sched_avg_update(rq);
+		delta = time - rq->age_stamp;
+		if (unlikely(delta < 0))
+			delta = 0;
+		rt = div64_u64(rq->rt_avg, sched_avg_period() + delta);
+		rt = (rt * max_cap) >> SCHED_CAPACITY_SHIFT;
+		*util = min(*util + rt, max_cap);
+	}
 }
 
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
@@ -375,7 +384,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		sg_policy->cached_raw_freq = 0;
 		next_f = policy->cpuinfo.max_freq;
 	} else {
-		sugov_get_util(&util, &max, sg_cpu->cpu);
+		sugov_get_util(&util, &max, sg_cpu->cpu, time);
 		util = uclamp_util(cpu_rq(sg_cpu->cpu), util);
 		sugov_iowait_boost(sg_cpu, &util, &max);
 		next_f = get_next_freq(sg_policy, util, max);
@@ -470,7 +479,7 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 	unsigned long util, max;
 	unsigned int next_f;
 
-	sugov_get_util(&util, &max, sg_cpu->cpu);
+	sugov_get_util(&util, &max, sg_cpu->cpu, time);
 
 	raw_spin_lock(&sg_policy->update_lock);
 
@@ -922,10 +931,7 @@ static int sugov_start(struct cpufreq_policy *policy)
 		sg_cpu->cpu = cpu;
 		sg_cpu->sg_policy = sg_policy;
 		sg_cpu->flags = SCHED_CPUFREQ_DL;
-		sg_cpu->iowait_boost_max = capacity_orig_of(cpu);
-		sg_cpu->min_boost =
-			(SCHED_CAPACITY_SCALE * policy->cpuinfo.min_freq) /
-			policy->cpuinfo.max_freq;
+		sg_cpu->iowait_boost_max = policy->cpuinfo.max_freq;
 	}
 
 	for_each_cpu(cpu, policy->cpus) {
