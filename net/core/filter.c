f6dd26a19559fed4861648e412c440f2bfd50ee2
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-10-30 13:46:47 -0700

    UPSTREAM: net: filter: remove unused variable and fix warning
    bpf_getsockopt bpf call sets the ret variable to zero and
    never changes it.  What's worse in case CONFIG_INET is
    not selected the variable is completely unused generating
    a warning.
    
    Change-Id: I754b9559dd1bfb166d495ab9bd63850c875530e3
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Lawrence Brakmo <brakmo@fb.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3379,7 +3379,6 @@ BPF_CALL_5(bpf_getsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 	   int, level, int, optname, char *, optval, int, optlen)
 {
 	struct sock *sk = bpf_sock->sk;
-	int ret = 0;
 
 	if (!sk_fullsock(sk))
 		goto err_clear;
@@ -3399,7 +3398,7 @@ BPF_CALL_5(bpf_getsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 	} else {
 		goto err_clear;
 	}
-	return ret;
+	return 0;
 #endif
 err_clear:
 	memset(optval, 0, optlen);
