315d6752c9e43a572694ba5f629e1c4057dbcc33
Ido Schimmel <idosch@mellanox.com>
2018-01-07 12:45:05 +0200

    UPSTREAM: ipv6: Set nexthop flags upon carrier change
    Similar to IPv4, when the carrier of a netdev changes we should toggle
    the 'linkdown' flag on all the nexthops using it as their nexthop
    device.
    
    This will later allow us to test for the presence of this flag during
    route lookup and dump.
    
    Up until commit 4832c30d5458 ("net: ipv6: put host and anycast routes on
    device with address") host and anycast routes used the loopback netdev
    as their nexthop device and thus were not marked with the 'linkdown'
    flag. The patch preserves this behavior and allows one to ping the local
    address even when the nexthop device does not have a carrier and the
    'ignore_routes_with_linkdown' sysctl is set.
    
    Change-Id: Iebd5598504278be473c009a17457e994547356f5
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Acked-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ip6_route.h
+++ include/net/ip6_route.h
@@ -183,6 +183,7 @@ void rt6_remove_prefsrc(struct inet6_ifaddr *ifp);
 void rt6_clean_tohost(struct net *net, struct in6_addr *gateway);
 void rt6_sync_up(struct net_device *dev, unsigned int nh_flags);
 void rt6_disable_ip(struct net_device *dev, unsigned long event);
+void rt6_sync_down_dev(struct net_device *dev, unsigned long event);
 
 void rt6_uncached_list_add(struct rt6_info *rt);
 void rt6_uncached_list_del(struct rt6_info *rt);
