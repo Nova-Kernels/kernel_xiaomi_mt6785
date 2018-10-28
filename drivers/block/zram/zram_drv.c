8118535d1570cb85d7b6e664af8d07799302c0d8
Nathan Chancellor <natechancellor@gmail.com>
2018-10-28 10:14:01 -0700

    zram: Move default compression algorithm choice to Kconfig
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,7 +43,7 @@ static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
 static struct zram *zram_devices;
-static const char *default_compressor = "lz4";
+static const char *default_compressor = CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM;
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
