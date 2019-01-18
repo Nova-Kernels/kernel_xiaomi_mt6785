cc97dd7225d2e10909774f3e691838756deda54d
Jaroslav Zvezda <acroreiser@gmail.com>
2019-01-19 00:00:29 +0200

    zram: use lz4 compressor by default if available

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,7 +43,11 @@ static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
 static struct zram *zram_devices;
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo";
+#endif
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
