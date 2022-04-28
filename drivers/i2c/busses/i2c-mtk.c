1e0efe731a4a0feef04ffb133fd9ec534b3ca104
VoidDev-0 <dev.avax@protonmail.com>
2022-04-28 18:50:41 +0200

    drivers/i2c: i2c-mtk: Remove unused variable
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- drivers/i2c/busses/i2c-mtk.c
+++ drivers/i2c/busses/i2c-mtk.c
@@ -1755,12 +1755,6 @@ err_clk_set_main:
 #endif
 }
 
-
-static struct syscore_ops mtk_i2c_syscore_ops = {
-	.resume = mt_i2c_pll_resume,
-	.suspend = mt_i2c_pll_suspend,
-};
-
 #ifdef CONFIG_PM_SLEEP
 
 static int mt_i2c_suspend_noirq(struct device *dev)
