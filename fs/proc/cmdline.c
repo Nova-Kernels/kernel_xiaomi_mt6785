00e3b5024f128e8ebe7d100b26b0c3fdbb1b05b7
ZyCromerZ <neetroid97@gmail.com>
2022-06-17 17:42:07 +0700

    cmdline: add more check flags before force it enforcing
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -80,9 +80,11 @@ static void patch_begonia_cmdline(char *cmdline)
 	}
 
 	// Thank you Xiaomi, very cool
-	if(check_flag(cmdline, "androidboot.selinux=", "permissive")) {
-		remove_flag(cmdline, "androidboot.selinux=");
-		append_cmdline(cmdline, "androidboot.selinux=enforcing");
+	if(check_flag(cmdline, "androidboot.forceenforcing=", "y")) {
+		if(check_flag(cmdline, "androidboot.selinux=", "permissive")) {
+			remove_flag(cmdline, "androidboot.selinux=");
+			append_cmdline(cmdline, "androidboot.selinux=enforcing");
+		}
 	}
 }
 #endif
