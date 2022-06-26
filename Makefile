4dbf7d33f50d0ac00c9b51cf4e793b9c7429b7a8
Wahid7852 <demolishercrazy@gmail.com>
2022-06-26 14:59:35 +0530

    Makefile: Added ldflags to avoid ld.lld invalid symbol name offset error

--- Makefile
+++ Makefile
@@ -757,7 +757,8 @@ endif
 KBUILD_CFLAGS += $(call cc-disable-warning, unused-but-set-variable)
 
 ifeq ($(ld-name),lld)
-LDFLAGS += -O2
+KBUILD_LDFLAGS  += --lto-O3
+LDFLAGS += --lto-O3
 endif
 
 KBUILD_CFLAGS += $(call cc-disable-warning, unused-const-variable)
---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jun 26 02:09:20 IST 2022"
+#define LINUX_COMPILE_BY "Abdul7852"
+#define LINUX_COMPILE_HOST "-Stable"
+#define LINUX_COMPILER "Azure clang version 15.0.0 (https://github.com/llvm/llvm-project 36c36c1ee3b01b2d4a9e7ee12c9c23a98659e0fb), LLD 15.0.0"
