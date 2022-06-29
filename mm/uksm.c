cd3863a78f87d3d037d17122752f1d7188bdfebb
ZyCromerZ <neetroid97@gmail.com>
2022-06-29 12:58:27 +0700

    mm: uksm: set uksm_cpu_governor to quiet by default
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- mm/uksm.c
+++ mm/uksm.c
@@ -528,7 +528,7 @@ static unsigned int uksm_sleep_saved;
 /* Max percentage of cpu utilization ksmd can take to scan in one batch */
 static unsigned int uksm_max_cpu_percentage;
 
-static int uksm_cpu_governor = 1;
+static int uksm_cpu_governor = 3;
 
 static char *uksm_cpu_governor_str[4] = { "full", "medium", "low", "quiet" };
 
