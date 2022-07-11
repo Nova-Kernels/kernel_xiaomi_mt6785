cf0df9de32bc9789e8bf8ac4f0228a5d1e990d81
“Wahid7852” <demolishercrazy@gmail.com>
2022-07-11 23:15:11 +0530

    Revert "simple_lmk: Report mm as freed as soon as exit_mmap() finishes"
    This reverts commit 46d15245647403aca02ebd6c569cd1cfd77f7a67.

--- kernel/fork.c
+++ kernel/fork.c
@@ -976,7 +976,6 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
-	simple_lmk_mm_freed(mm);
 	mm_put_huge_zero_page(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
