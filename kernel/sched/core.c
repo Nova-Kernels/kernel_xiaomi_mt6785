9d0a08c4fb5740dd110edbfbd0ed167228534c9f
“Wahid7852” <demolishercrazy@gmail.com>
2022-07-20 12:04:59 +0530

    sched/core.c: Removed Unused Variable

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4648,7 +4648,6 @@ static noinline void __schedule_bug(struct task_struct *prev)
 {
 	/* Save this before calling printk(), since that will clobber it */
 	unsigned long preempt_disable_ip = get_preempt_disable_ip(current);
-	int i = 0;
 	if (oops_in_progress)
 		return;
 
