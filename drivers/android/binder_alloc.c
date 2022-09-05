b652f108f35e3f1713d6067bf4448812eea1f6dd
VoidDev-0 <dev.avax@protonmail.com>
2022-09-05 22:33:05 +0200

    drivers: binder: Remove obsolete ifdef
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -50,7 +50,6 @@ static uint32_t binder_alloc_debug_mask = BINDER_DEBUG_USER_ERROR;
 module_param_named(debug_mask, binder_alloc_debug_mask,
 		   uint, 0644);
 
-#ifdef CONFIG_DEBUG_KERNEL
 #define binder_alloc_debug(mask, x...) \
 	do { \
 		if (binder_alloc_debug_mask & mask) \
