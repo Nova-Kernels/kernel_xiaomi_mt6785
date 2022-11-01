66c2cd875d5ca069c374a5cb3645041d16b9c987
Wahid7852 <wahidzk0091@gmail.com>
2022-11-01 23:54:53 +0530

    drivers: usb/host: added missing function

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -2026,7 +2026,9 @@ void xhci_free_command(struct xhci_hcd *xhci,
 
 /* xHCI host controller glue */
 typedef void (*xhci_get_quirks_t)(struct device *, struct xhci_hcd *);
-int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, int usec);
+int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, u64 timeout_us);
+int xhci_handshake_check_state(struct xhci_hcd *xhci,
+		void __iomem *ptr, u32 mask, u32 done, int usec);
 void xhci_quiesce(struct xhci_hcd *xhci);
 int xhci_halt(struct xhci_hcd *xhci);
 int xhci_start(struct xhci_hcd *xhci);
