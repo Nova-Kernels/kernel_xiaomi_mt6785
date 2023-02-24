36ae2a46705138f4d9cc17b389b9a6b4d46abec5
VoidDev-0 <dev.avax@protonmail.com>
2023-02-24 17:41:19 +0100

    lib/ratelimit: Don't print out suppressed callbacks
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- lib/ratelimit.c
+++ lib/ratelimit.c
@@ -54,9 +54,6 @@ int ___ratelimit(struct ratelimit_state *rs, const char *func)
 	if (time_is_before_jiffies(rs->begin + interval)) {
 		if (rs->missed) {
 			if (!(rs->flags & RATELIMIT_MSG_ON_RELEASE)) {
-				printk_deferred(KERN_WARNING
-						"%s: %d callbacks suppressed\n",
-						func, rs->missed);
 				rs->missed = 0;
 			}
 		}
