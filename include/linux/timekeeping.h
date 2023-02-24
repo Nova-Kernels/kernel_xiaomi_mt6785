2b0ea45e8def15821c5fbd2f4475f7aa967f819a
Arnd Bergmann <arnd@arndb.de>
2023-02-24 17:15:16 +0100

    timekeeping: Clean up ktime_get_real_ts64
    In a move to make ktime_get_*() the preferred driver interface into the
    timekeeping code, sanitizes ktime_get_real_ts64() to be a proper exported
    symbol rather than an alias for getnstimeofday64().
    
    The internal __getnstimeofday64() is no longer used, so remove that
    and merge it into ktime_get_real_ts64().
    
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -41,12 +41,17 @@ static inline struct timespec current_kernel_time(void)
 struct timespec64 get_monotonic_coarse64(void);
 extern void getrawmonotonic64(struct timespec64 *ts);
 extern void ktime_get_ts64(struct timespec64 *ts);
+extern void ktime_get_real_ts64(struct timespec64 *tv);
 extern time64_t ktime_get_seconds(void);
 extern time64_t ktime_get_real_seconds(void);
 
-extern int __getnstimeofday64(struct timespec64 *tv);
-extern void getnstimeofday64(struct timespec64 *tv);
 extern void getboottime64(struct timespec64 *ts);
+
+/*
+ * deprecated aliases, don't use in new code
+ */
+#define getnstimeofday64(ts)		ktime_get_real_ts64(ts)
+
 
 #if BITS_PER_LONG == 64
 /**
@@ -59,7 +64,7 @@ static inline int do_settimeofday(const struct timespec *ts)
 
 static inline int __getnstimeofday(struct timespec *ts)
 {
-	return __getnstimeofday64(ts);
+	return 0;
 }
 
 static inline void getnstimeofday(struct timespec *ts)
@@ -157,8 +162,6 @@ static inline void getboottime(struct timespec *ts)
 	*ts = timespec64_to_timespec(ts64);
 }
 #endif
-
-#define ktime_get_real_ts64(ts)	getnstimeofday64(ts)
 
 /*
  * ktime_t based interfaces
@@ -346,6 +349,5 @@ extern void read_persistent_clock64(struct timespec64 *ts);
 extern void read_boot_clock64(struct timespec64 *ts);
 extern int update_persistent_clock(struct timespec now);
 extern int update_persistent_clock64(struct timespec64 now);
-
 
 #endif
