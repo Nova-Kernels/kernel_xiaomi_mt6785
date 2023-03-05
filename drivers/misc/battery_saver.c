6f7f2ac4b67f819bdc8f9bb8ee208648a113bbc5
Yaroslav Zviezda <acroreiser@gmail.com>
2023-03-05 17:42:45 +0300

    battery_saver: convert to module_param_cb()

--- drivers/misc/battery_saver.c
+++ drivers/misc/battery_saver.c
@@ -8,7 +8,6 @@
 #include <linux/battery_saver.h>
 
 static bool enabled = false;
-module_param(enabled, bool, 0644);
 
 // returns whether battery saver is enabled or disabled
 bool is_battery_saver_on(void)
@@ -21,3 +20,35 @@ void update_battery_saver(bool status)
 {
 	enabled = status;
 }
+
+static int set_enabled(const char *val, const struct kernel_param *kp)
+{
+	long state;
+	int res = 0;
+
+	if(!strcmp(val, "Y"))
+		state = 1;
+	else if(!strcmp(val, "N"))
+		state = 0;
+	else
+		res = kstrtol(val, 0, &state);
+
+	if (res != 0 || state < 0 || state > 1)
+		return -EINVAL;
+
+	if(state)
+		printk(KERN_INFO "Entering battery saving mode\n");
+	else
+		printk(KERN_INFO "Leaving battery saving mode\n");
+
+	enabled = state;
+	return 0;
+}
+
+static const struct kernel_param_ops battery_saver_ops =
+{
+	.set = &set_enabled,
+	.get = &param_get_bool,
+};
+
+module_param_cb(enabled, &battery_saver_ops, &enabled, S_IRUGO|S_IWUSR );
