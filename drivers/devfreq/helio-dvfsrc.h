c2b0510ca4b37c645a9beae6192d1fbbbf2da993
KiGamji <maloyh44@gmail.com>
2023-03-30 13:04:32 +0500

    UNFINISHED: treewide: Get rid of additional drivers for all SOCs except mt6785
    Signed-off-by: KiGamji <maloyh44@gmail.com>

--- drivers/devfreq/helio-dvfsrc.h
+++ drivers/devfreq/helio-dvfsrc.h
@@ -12,18 +12,6 @@
  */
 
 
-#if defined(CONFIG_MACH_MT6768) || defined(CONFIG_MACH_MT6765)
-#include <helio-dvfsrc_v2.h>
-#elif defined(CONFIG_MACH_MT6785) || defined(CONFIG_MACH_MT6885)
+#if defined(CONFIG_MACH_MT6785)
 #include <helio-dvfsrc_v3.h>
-#elif defined(CONFIG_MACH_MT6873)
-#include <helio-dvfsrc_v3.h>
-#elif defined(CONFIG_MACH_MT6853)
-#include <helio-dvfsrc_v3.h>
-#elif defined(CONFIG_MACH_MT6893)
-#include <helio-dvfsrc_v3.h>
-#elif defined(CONFIG_MACH_MT6833)
-#include <helio-dvfsrc_v3.h>
-#else
-#include <helio-dvfsrc_v1.h>
 #endif
