51f36dd79ef0e972d1c4bb5fde235c5f2705f112
Kir Kolyshkin <kolyshkin@gmail.com>
2023-08-07 20:03:57 -0700

    sched/headers: Move 'struct sched_param' out of uapi, to work around glibc/musl breakage
    [ Upstream commit d844fe65f0957024c3e1b0bf2a0615246184d9bc ]
    
    Both glibc and musl define 'struct sched_param' in sched.h, while kernel
    has it in uapi/linux/sched/types.h, making it cumbersome to use
    sched_getattr(2) or sched_setattr(2) from userspace.
    
    For example, something like this:
    
    	#include <sched.h>
    	#include <linux/sched/types.h>
    
    	struct sched_attr sa;
    
    will result in "error: redefinition of ‘struct sched_param’" (note the
    code doesn't need sched_param at all -- it needs struct sched_attr
    plus some stuff from sched.h).
    
    The situation is, glibc is not going to provide a wrapper for
    sched_{get,set}attr, thus the need to include linux/sched_types.h
    directly, which leads to the above problem.
    
    Thus, the userspace is left with a few sub-par choices when it wants to
    use e.g. sched_setattr(2), such as maintaining a copy of struct
    sched_attr definition, or using some other ugly tricks.
    
    OTOH, 'struct sched_param' is well known, defined in POSIX, and it won't
    be ever changed (as that would break backward compatibility).
    
    So, while 'struct sched_param' is indeed part of the kernel uapi,
    exposing it the way it's done now creates an issue, and hiding it
    (like this patch does) fixes that issue, hopefully without creating
    another one: common userspace software rely on libc headers, and as
    for "special" software (like libc), it looks like glibc and musl
    do not rely on kernel headers for 'struct sched_param' definition
    (but let's Cc their mailing lists in case it's otherwise).
    
    The alternative to this patch would be to move struct sched_attr to,
    say, linux/sched.h, or linux/sched/attr.h (the new file).
    
    Oh, and here is the previous attempt to fix the issue:
    
      https://lore.kernel.org/all/20200528135552.GA87103@google.com/
    
    While I support Linus arguments, the issue is still here
    and needs to be fixed.
    
    [ mingo: Linus is right, this shouldn't be needed - but on the other
             hand I agree that this header is not really helpful to
    	 user-space as-is. So let's pretend that
    	 <uapi/linux/sched/types.h> is only about sched_attr, and
    	 call this commit a workaround for user-space breakage
    	 that it in reality is ... Also, remove the Fixes tag. ]
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20230808030357.1213829-1-kolyshkin@gmail.com
    Stable-dep-of: 0664e2c311b9 ("sched/deadline: Fix warning in migrate_enable for boosted tasks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -47,7 +47,6 @@ struct rcu_node;
 struct reclaim_state;
 struct robust_list_head;
 struct sched_attr;
-struct sched_param;
 struct seq_file;
 struct sighand_struct;
 struct signal_struct;
@@ -300,6 +299,10 @@ enum uclamp_id {
 
 	/* Utilization clamping constraints count */
 	UCLAMP_CNT
+};
+
+struct sched_param {
+	int sched_priority;
 };
 
 struct sched_info {
