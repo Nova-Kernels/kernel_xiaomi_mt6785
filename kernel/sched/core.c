8faa993df4b24867bfae9323f1b4f927fd45cd7d
Wahid Khan <wahidzk0091@gmail.com>
2023-08-11 10:10:40 +0530

    sched: Set sched_nr_migrate to 32
    the original commit by kerneltoast was for systems having RT. While the config for full RT was not defined, I derped out and made it not defined for android so lets fix the derp
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -89,7 +89,7 @@ const_debug unsigned int sysctl_sched_features =
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
-#if !defined(CONFIG_ANDROID)
+#if defined(CONFIG_ANDROID)
 const_debug unsigned int sysctl_sched_nr_migrate = 32;
 #else
 const_debug unsigned int sysctl_sched_nr_migrate = 8;
