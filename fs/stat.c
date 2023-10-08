8c6580070854fb81f4d37f7dcea68e1d7204fa1e
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-10-08 11:00:19 +0000

    fs: Guard KSU Related Code

--- fs/stat.c
+++ fs/stat.c
@@ -147,9 +147,9 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 	return error;
 }
 EXPORT_SYMBOL(vfs_statx_fd);
-
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-
+#ifdef CONFIG_KSU
+	extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
@@ -171,8 +171,9 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-
+#ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flags);
+#endif	
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
