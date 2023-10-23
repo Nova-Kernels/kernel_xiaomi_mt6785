bd71e0adbe2c20c3bcaaed558354a00019bf9e5c
bengris32 <bengris32@protonmail.ch>
2023-10-23 18:40:54 +0100

    performance: Fix build breakage when touch boost and fpsgo are enabled
    Change-Id: I26c71cd4073dfb133e86c420acbbb648415f3c74
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/performance/perfmgr_main.c
+++ drivers/misc/mediatek/performance/perfmgr_main.c
@@ -39,7 +39,7 @@ struct platform_device perfmgr_device = {
 
 static int perfmgr_suspend(struct device *dev)
 {
-#ifdef CONFIG_MTK_PERFMGR_TOUCH_BOOST
+#if defined(CONFIG_MTK_PERFMGR_TOUCH_BOOST) && !(defined(CONFIG_MTK_FPSGO) || defined(CONFIG_MTK_FPSGO_V3))
 	ktch_suspend();
 #endif
 	return 0;
