4659baa1bfe6030d3029e57a020f46a2262ea388
Wahid Khan <wahidzk0091@gmail.com>
2023-11-06 17:15:43 +0000

    [SQUASH] KernelSU: implement KernelSU
    This is a squash of previously poorly implemented KernelSU instances
    
    Revert "KernelSU: update"
    
    This reverts commit 1b651f565bc0a69186d0a0842aa5023803cea3f2.
    
    Revert "KernelSU: Update"
    
    This reverts commit cb7e45deec5feb28f7de12a7b1b09c8a445f9910.
    
    KernelSU: Added KernelSU Support
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    
    fs: Use KernelSU Second Way Implementation
    - Seems KProbes is broken.
    - Based on https://kernelsu.org/guide/how-to-integrate-for-non-gki.html.
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Change-Id: Id9d887b069aa4c0e63d7579b9529c0741d35d31f
    Signed-off-by: Neebe3289 <neebexd@gmail.com>
    
    Kernelsu: update
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    
    defconfig: disable kprobes
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1741,6 +1741,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1751,6 +1757,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
