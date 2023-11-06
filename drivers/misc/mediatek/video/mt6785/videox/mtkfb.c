9aca2ec1f6e70464e0ee88fe63346590a5dc5484
zainarbani <zaintsyariev@gmail.com>
2023-11-06 13:42:57 +0700

    drivers/misc/mediatek/video: fix primary panel resume race and remove leak
    Signed-off-by: Wahid7852 <wahidzk0091@gmail.com>

--- drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
@@ -2668,14 +2668,6 @@ static int mtkfb_probe(struct platform_device *pdev)
 	fbdev->dev = &(pdev->dev);
 	dev_set_drvdata(&(pdev->dev), fbdev);
 
-	atomic_set(&fbdev->resume_pending, 0);
-	init_waitqueue_head(&fbdev->resume_wait_q);
-
-	fbdev->is_prim_panel = true;
-	prim_fbi = fbi;
-	atomic_set(&prim_panel_is_on, false);
-	INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
-
 	DISPMSG("%s: fb_pa = %pa\n", __func__, &fb_base);
 
 #ifdef CONFIG_MTK_IOMMU_V2
@@ -2815,6 +2807,13 @@ static int mtkfb_probe(struct platform_device *pdev)
 #endif
 	fbdev->state = MTKFB_ACTIVE;
 
+	atomic_set(&fbdev->resume_pending, 0);
+	init_waitqueue_head(&fbdev->resume_wait_q);
+	fbdev->is_prim_panel = true;
+	prim_fbi = fbi;
+	atomic_set(&prim_panel_is_on, false);
+	INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
+
 	MSG_FUNC_LEAVE();
 	pr_info("disp driver(2) %s end\n", __func__);
 	return 0;
@@ -2834,6 +2833,9 @@ static int mtkfb_remove(struct platform_device *pdev)
 
 	MSG_FUNC_ENTER();
 	/* FIXME: wait till completion of pending events */
+
+	atomic_set(&prim_panel_is_on, false);
+	cancel_delayed_work(&prim_panel_work);
 
 	fbdev->state = MTKFB_DISABLED;
 	mtkfb_free_resources(fbdev, saved_state);
@@ -3170,9 +3172,13 @@ int mtkfb_prim_panel_unblank(int timeout)
 
 	if (prim_fbi) {
 		fbdev = (struct mtkfb_device *)prim_fbi->par;
-		wait_event_timeout(fbdev->resume_wait_q,
+		ret = wait_event_timeout(fbdev->resume_wait_q,
 				!atomic_read(&fbdev->resume_pending),
 				msecs_to_jiffies(WAIT_RESUME_TIMEOUT));
+		if (!ret) {
+			printk("Primary fb resume timeout\n");
+			return -ETIMEDOUT;
+		}
 		if (!lock_fb_info(prim_fbi)) {
 			return -ENODEV;
 		}
