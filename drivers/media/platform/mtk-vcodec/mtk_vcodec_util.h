cdd23736cb1a98334a051b6c1e47d96e2cc10c0a
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-12-20 15:51:44 +0000

    mtk-vcodec: Set `DEBUG` Flag to 0 and use the proper printing apis.
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_util.h
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_util.h
@@ -56,14 +56,14 @@ extern int mtk_v4l2_dbg_level;
 extern bool mtk_vcodec_dbg;
 extern bool mtk_vcodec_perf;
 
-#define DEBUG   1
+#define DEBUG   0
 
 #if defined(DEBUG)
 
 #define mtk_v4l2_debug(level, fmt, args...)                              \
 	do {                                                             \
 		if ((mtk_v4l2_dbg_level & level) == level)           \
-			pr_info("[MTK_V4L2] level=%d %s(),%d: " fmt "\n",\
+			pr_debug("[MTK_V4L2] level=%d %s(),%d: " fmt "\n",\
 				level, __func__, __LINE__, ##args);      \
 	} while (0)
 
@@ -78,7 +78,7 @@ extern bool mtk_vcodec_perf;
 #define mtk_vcodec_debug(h, fmt, args...)                               \
 	do {                                                            \
 		if (mtk_vcodec_dbg)                                  \
-			pr_info("[MTK_VCODEC][%d]: %s() " fmt "\n",     \
+			pr_debug("[MTK_VCODEC][%d]: %s() " fmt "\n",     \
 				((struct mtk_vcodec_ctx *)h->ctx)->id,  \
 				__func__, ##args);                      \
 	} while (0)
@@ -86,12 +86,12 @@ extern bool mtk_vcodec_perf;
 #define mtk_vcodec_perf_log(fmt, args...)                               \
 	do {                                                            \
 		if (mtk_vcodec_perf)                          \
-			pr_info("[MTK_PERF] " fmt "\n", ##args);        \
+			pr_debug("[MTK_PERF] " fmt "\n", ##args);        \
 	} while (0)
 
 
 #define mtk_vcodec_err(h, fmt, args...)                                 \
-	pr_info("[MTK_VCODEC][ERROR][%d]: %s() " fmt "\n",               \
+	pr_err("[MTK_VCODEC][ERROR][%d]: %s() " fmt "\n",               \
 		   ((struct mtk_vcodec_ctx *)h->ctx)->id, __func__, ##args)
 
 #define mtk_vcodec_debug_enter(h)  mtk_vcodec_debug(h, "+")
