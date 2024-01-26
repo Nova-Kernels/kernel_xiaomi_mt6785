b6a02da6f6f961a18215ebc105a27b5211b3f887
bengris32 <bengris32@protonmail.ch>
2024-01-26 19:56:15 +0000

    cpu-boost: Adapt to MediaTek's sched_boost implementation
    Change-Id: I9d703cb926395b3ea4f7d98226bbdfb40c6bb5cc
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -48,6 +48,8 @@ struct cpu_sync {
 	unsigned int input_boost_freq;
 };
 
+extern int set_sched_boost_type(int type);
+
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 
 static struct kthread_work input_boost_work;
@@ -189,7 +191,7 @@ static void update_policy_online(void)
 
 static void do_input_boost_rem(struct work_struct *work)
 {
-	unsigned int i, ret;
+	unsigned int i;
 	struct cpu_sync *i_sync_info;
 
 	/* Reset the input_boost_min for all CPUs in the system */
@@ -203,21 +205,19 @@ static void do_input_boost_rem(struct work_struct *work)
 	update_policy_online();
 
 	if (sched_boost_active) {
-		ret = sched_set_boost(0);
-		if (ret)
-			pr_err("cpu-boost: sched boost disable failed\n");
+		set_sched_boost_type(0); /* SCHED_NO_BOOST */
 		sched_boost_active = false;
 	}
 }
 
 static void do_input_boost(struct kthread_work *work)
 {
-	unsigned int i, ret;
+	unsigned int i;
 	struct cpu_sync *i_sync_info;
 
 	cancel_delayed_work_sync(&input_boost_rem);
 	if (sched_boost_active) {
-		sched_set_boost(0);
+		set_sched_boost_type(0); /* SCHED_NO_BOOST */
 		sched_boost_active = false;
 	}
 
@@ -232,12 +232,9 @@ static void do_input_boost(struct kthread_work *work)
 	update_policy_online();
 
 	/* Enable scheduler boost to migrate tasks to big cluster */
-	if (sched_boost_on_input > 0) {
-		ret = sched_set_boost(sched_boost_on_input);
-		if (ret)
-			pr_err("cpu-boost: sched boost enable failed\n");
-		else
-			sched_boost_active = true;
+	if (sched_boost_on_input) {
+		set_sched_boost_type(1); /* SCHED_ALL_BOOST */
+		sched_boost_active = true;
 	}
 
 	schedule_delayed_work(&input_boost_rem, msecs_to_jiffies(input_boost_ms));
