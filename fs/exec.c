6cb53b7e2da6c31f0907826e3a7b77f4d659fa85
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-02 16:00:48 +0800

    KernelSU: manual filesystem hooks
    https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1706,6 +1706,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			                   void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1719,6 +1724,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
