ecd540ce49e3c27e3eca561b71b657996a19f8bf
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-05-14 22:40:00 +0800

    KernelSU: devpts hook
    https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#failed-to-execute-pm-in-terminal
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -602,6 +602,8 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
+extern int ksu_handle_devpts(struct inode*);
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -610,6 +612,7 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
+	ksu_handle_devpts(dentry->d_inode);
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
