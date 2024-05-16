853810facd43443b87dd2bf084020082d74f0fe3
Tim Zimmermann <tim@linux4.de>
2024-05-16 05:56:11 +0200

    syscall: Increase bpf fake uname to 5.10.246
    * We have traced backport of the 5.10 BPF patch set, so now we can use
      it
    * Including the subsequent startup of QPR3 and Android17
    * Temporarily get rid of override kernel BPF version
    
    Change-Id: Iaba91f5594cebd2e361b670fb866abb5c58c6707

--- kernel/sys.c
+++ kernel/sys.c
@@ -1191,6 +1191,14 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+    if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
+		strcpy(tmp.release, "5.10.246");
+		pr_debug("fake uname: %s release=%s\n",
+			 current->comm, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
