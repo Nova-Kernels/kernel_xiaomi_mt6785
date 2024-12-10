19c459615798a2559e5a69047305865a201c4011
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2024-12-10 23:46:38 +0800

    fs: Add close_fd_get_file() for binder
    * Extracted from upstream commit [1].
    * Combined with commit [2] and [3].
    
    [1]: https://github.com/torvalds/linux/commit/80cd795630d6526ba729a089a435bf74a57af927
    [2]: https://github.com/torvalds/linux/commit/6e802a4ba056a6f2f51ac9d54eead3ed6f9829a2
    [3]: https://github.com/torvalds/linux/commit/9fe83c43e71cdb8e5b9520bcb98706a2b3c680c8
    
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
    Signed-off-by: cvnertnc <148134890+cvnertnc@users.noreply.github.com>

--- include/linux/fdtable.h
+++ include/linux/fdtable.h
@@ -125,6 +125,7 @@ extern void __fd_install(struct files_struct *files,
 extern int __close_fd(struct files_struct *files,
 		      unsigned int fd);
 extern int __close_range(unsigned int fd, unsigned int max_fd, unsigned int flags);
+extern int close_fd_get_file(unsigned int fd, struct file **res);
 
 extern struct kmem_cache *files_cachep;
 
