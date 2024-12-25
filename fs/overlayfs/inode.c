fd69720b47ebd154d9fc2fecf4166f4fbd50c8a2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-25 18:59:53 +0800

    susfs: overlayfs/inode: fixup!
    ../fs/overlayfs/inode.c:76:36: error: too few arguments to function call, expected 4, have 2
       76 |                 err = vfs_getattr(&realpath, stat);
          |                       ~~~~~~~~~~~                ^
    ../include/linux/fs.h:3150:12: note: 'vfs_getattr' declared here
     3150 | extern int vfs_getattr(const struct path *, struct kstat *, u32, unsigned int);
          |            ^           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -73,9 +73,8 @@ int ovl_getattr(const struct path *path, struct kstat *stat,
 	ovl_path_lowerdata(dentry, &realpath);
 	if (likely(realpath.mnt && realpath.dentry)) {
 		old_cred = ovl_override_creds(dentry->d_sb);
-		err = vfs_getattr(&realpath, stat);
-		ovl_revert_creds(old_cred);
-		return err;
+		err = vfs_getattr(&realpath, stat, request_mask, flags);
+		goto out;
 	}
 #endif
 	type = ovl_path_real(dentry, &realpath);
