9bc95dcf19cd55eedcb16e0dd8b4417ba752f5b6
WuXing90 <2096477910@qq.com>
2026-01-29 22:28:26 +0800

    fs/fuse: Remove duplicate structures
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- fs/fuse/passthrough.c
+++ fs/fuse/passthrough.c
@@ -12,17 +12,6 @@ struct fuse_aio_req {
 	struct kiocb *iocb_fuse;
 };
 
-static inline void kiocb_clone(struct kiocb *kiocb, struct kiocb *kiocb_src,
-					struct file *filp)
-{
-	*kiocb = (struct kiocb){
-		.ki_filp = filp,
-		.ki_flags = kiocb_src->ki_flags,
-		.ki_hint = kiocb_src->ki_hint,
-		.ki_pos = kiocb_src->ki_pos,
-	};
-}
-
 static void fuse_file_accessed(struct file *dst_file, struct file *src_file)
 {
 	struct inode *dst_inode;
