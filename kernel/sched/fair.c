e26fa703b9d3ec5075b940ee7803f079ebf29eab
Wahid7852 <wahidzk0091@gmail.com>
2026-07-04 06:13:58 +0530

    BACKPORT: sched/pelt: Skip updating util_est when utilization is higher than CPU's capacity
    Under thermal capping a task's measured utilization can exceed
    capacity_orig_of(cpu); sampling that into util_est inflates the
    estimate and can produce a spurious frequency spike once the cap
    lifts. Skip the sample in that case.
    
    (cherry picked from commit 10a35e6812aa0953f02a956c499d23fe4e68af4a)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3838,6 +3838,13 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 		return;
 
 	/*
+	 * To avoid overestimation of actual task utilization, skip updates if
+	 * we cannot grant there is idle time in this CPU.
+	 */
+	if (task_util(p) > capacity_orig_of(cpu_of(rq_of(cfs_rq))))
+		return;
+
+	/*
 	 * Update Task's estimated utilization
 	 *
 	 * When *p completes an activation we can consolidate another sample
