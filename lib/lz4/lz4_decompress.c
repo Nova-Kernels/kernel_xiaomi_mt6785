7a5e004e20661424f006ff393a24b8c40637b7b9
hxsyzl <hxsyzl@ust.edu.kg>
2026-07-04 07:20:02 +0000

    Revert "lz4: Staticize some functions"
    This reverts commit d72fe37d63d9fa02e96f7f8533da4f59fc690527.
    
    Signed-off-by: cvnertnc <148134890+cvnertnc@users.noreply.github.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -138,7 +138,7 @@ static FORCE_INLINE void LZ4_memcpy_using_offset(BYTE *dstPtr,
  * does not know end of input
  * presumes input is well formed
  * note : will consume at least one byte */
-static size_t read_long_length_no_check(const BYTE **pp)
+size_t read_long_length_no_check(const BYTE **pp)
 {
 	size_t b, l = 0;
 	do {
@@ -901,7 +901,7 @@ int LZ4_decompress_safe_partial(const char *src, char *dst, int compressedSize,
 				      0);
 }
 
-static int LZ4_decompress_fast(const char *source, char *dest, int originalSize)
+int LZ4_decompress_fast(const char *source, char *dest, int originalSize)
 {
 	return LZ4_decompress_unsafe_generic((const BYTE *)source, (BYTE *)dest,
 					     originalSize, 0, NULL, 0);
@@ -928,7 +928,7 @@ static int LZ4_decompress_safe_withSmallPrefix(const char *source, char *dest,
 				      (BYTE *)dest - prefixSize, NULL, 0);
 }
 
-static int LZ4_decompress_safe_forceExtDict(const char *source, char *dest,
+int LZ4_decompress_safe_forceExtDict(const char *source, char *dest,
 				     int compressedSize, int maxOutputSize,
 				     const void *dictStart, size_t dictSize)
 {
@@ -1047,7 +1047,7 @@ int LZ4_decompress_safe_continue(LZ4_streamDecode_t *LZ4_streamDecode,
 	return result;
 }
 
-static int LZ4_decompress_fast_continue(LZ4_streamDecode_t *LZ4_streamDecode,
+int LZ4_decompress_fast_continue(LZ4_streamDecode_t *LZ4_streamDecode,
 				 const char *source, char *dest,
 				 int originalSize)
 {
@@ -1122,9 +1122,9 @@ static FORCE_INLINE int LZ4_decompress_usingDict_generic(const char *source,
 		(BYTE *)dest, (const BYTE *)dictStart, dictSize);
 }
 
-static int LZ4_decompress_safe_usingDict(const char *source, char *dest,
-				  int compressedSize, int maxOutputSize,
-				  const char *dictStart, int dictSize)
+int LZ4_decompress_safe_usingDict(const char *source, char *dest,
+	int compressedSize, int maxOutputSize,
+	const char *dictStart, int dictSize)
 {
 	if (dictSize == 0)
 		return LZ4_decompress_safe(source, dest, compressedSize,
@@ -1141,7 +1141,7 @@ static int LZ4_decompress_safe_usingDict(const char *source, char *dest,
 						dictSize);
 }
 
-static int LZ4_decompress_fast_usingDict(const char *source, char *dest,
+int LZ4_decompress_fast_usingDict(const char *source, char *dest,
 				  int originalSize, const char *dictStart,
 				  int dictSize)
 {
@@ -1157,8 +1157,12 @@ static int LZ4_decompress_fast_usingDict(const char *source, char *dest,
 #ifndef STATIC
 EXPORT_SYMBOL(LZ4_decompress_safe);
 EXPORT_SYMBOL(LZ4_decompress_safe_partial);
+EXPORT_SYMBOL(LZ4_decompress_fast);
 EXPORT_SYMBOL(LZ4_setStreamDecode);
 EXPORT_SYMBOL(LZ4_decompress_safe_continue);
+EXPORT_SYMBOL(LZ4_decompress_fast_continue);
+EXPORT_SYMBOL(LZ4_decompress_safe_usingDict);
+EXPORT_SYMBOL(LZ4_decompress_fast_usingDict);
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("LZ4 decompressor");
